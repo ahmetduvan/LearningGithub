ac8f44edf8581c0d104c07b313cb2bbbd02bc119
ahmetduvan <ahmetduvan@gmail.com>
2022-07-07 18:54:09 -0700

    add comment2

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5};
+/*
+        for (int = 0; i<array.length; i++) {
+            System.out.println(array[i] + " ");
+
+        }
+  */
+    }
+}
